0f6c0a754c0d0f284024aa43a791369d3834c65f
Sohail Mukhtiar <sohailmukhtiarali@gmail.com>
2026-01-03 20:23:43 +0500

    Update Student.js

--- Docker-Compose-Projects/frontend/src/Student.js
+++ Docker-Compose-Projects/frontend/src/Student.js
@@ -9,51 +9,62 @@ function Student() {
     rollNo: '',
     class: '',
   });
-  const [data, setData]= useState([])
 
-  const API_BASE_URL = process.env.REACT_APP_API_URL;;
+  const [data, setData] = useState([]);
 
-  console.log("API",API_BASE_URL)
-const getData=()=> {
-  fetch(`${API_BASE_URL}/student`)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log('Fetched Data:', data);
-    setData(data);
-  })
-  .catch((err) => console.log(err));
-}
+  const API_BASE_URL = process.env.REACT_APP_API_URL;
+  console.log("API:", API_BASE_URL);
 
-  useEffect(() => {
-    getData()
-  }, []); 
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setStudentData({ ...studentData, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setStudentData({
-      name: '',
-      rollNo: '',
-      class: '',
-    });
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-  ...studentData,
-  rollNo: Number(studentData.rollNo)   // 🔴 YAHAN FIX HAI
-})
-  };
-    
-    fetch(`${API_BASE_URL}/addstudent`, requestOptions ).then((res) => res.json())
-.then(() => {
-getData()})
+  // Fetch students
+  const getData = () => {
+    fetch(`${API_BASE_URL}/student`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Fetched Data:', data);
+        setData(data);
+      })
       .catch((err) => console.log(err));
   };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // Handle input change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setStudentData({
+      ...studentData,
+      [name]: value,
+    });
+  };
+
+  // Submit form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(studentData),
+    };
+
+    fetch(`${API_BASE_URL}/addstudent`, requestOptions)
+      .then((res) => res.json())
+      .then(() => {
+        getData();
+        setStudentData({
+          name: '',
+          rollNo: '',
+          class: '',
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // Delete student
   const handleDelete = (id) => {
     fetch(`${API_BASE_URL}/student/${id}`, {
       method: 'DELETE',
@@ -67,74 +78,99 @@ getData()})
 
   return (
     <div>
-    <div className="student-container">
-      <div className="content">
-        <h2 className='store-student-details'style={{ marginLeft: '100px' }}>Student Details</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={studentData.name}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Roll No (Must be Number):</label>
-            <input
-              type="text"
-              name="rollNo"
-              value={studentData.rollNo}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Class:</label>
-            <input
-              type="text"
-              name="class"
-              value={studentData.class}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="form-group">
-          <p style={{ marginLeft: '50px' }}>
-            <button type="submit">Submit</button>
-            </p>
-          </div>
-        </form>
+      <div className="student-container">
+        <div className="content">
+          <h2 className="store-student-details" style={{ marginLeft: '100px' }}>
+            Student Details
+          </h2>
+
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Name:</label>
+              <input
+                type="text"
+                name="name"
+                value={studentData.name}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Roll No (Must be Number):</label>
+              <input
+                type="text"
+                name="rollNo"
+                value={studentData.rollNo}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Class:</label>
+              <input
+                type="text"
+                name="class"
+                value={studentData.class}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <p style={{ marginLeft: '50px' }}>
+                <button type="submit">Submit</button>
+              </p>
+            </div>
+          </form>
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '50px 0' }}>
+          <h2 className="student-details">Student details</h2>
+
+          <table
+            className="student-table gradient-bg"
+            style={{
+              border: '1px solid #ccc',
+              borderCollapse: 'collapse',
+              margin: '100px 0',
+              marginLeft: '800px',
+              marginTop: '200px',
+              width: '40%',
+              backgroundColor: '#f4f4f4',
+            }}
+          >
+            <thead>
+              <tr style={{ backgroundColor: '#ddd' }}>
+                <th>StudentID</th>
+                <th>Name</th>
+                <th>Roll Number</th>
+                <th>Class</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {data.map((d, i) => (
+                <tr key={i}>
+                  <td>{d.id}</td>
+                  <td>{d.name}</td>
+                  <td>{d.roll_number}</td>
+                  <td>{d.class}</td>
+                  <td>
+                    <button
+                      className="delete-button"
+                      onClick={() => handleDelete(d.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '50px 0' }}>
-      <h2 className='student-details'>Student details</h2>
-      <table className="student-table gradient-bg" style={{ border: '1px solid #ccc', borderCollapse: 'collapse', margin: '100px 0',marginLeft: '800px', marginTop: '200px', width: '40%', backgroundColor: '#f4f4f4'}}>
-        <thead>
-          <tr style={{ backgroundColor: '#ddd' }}>
-          <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>StudentID</td>
-            <th style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>Name</th>
-            <th style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>Roll Number</th>
-            <th style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>Class</th>
-            <th style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((d, i) => (
-            <tr key={i} style={{ backgroundColor: i % 2 === 0 ? '#f9f9f9' : '#fff' }}>
-              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>{d.id}</td>
-              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>{d.name}</td>
-              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>{d.roll_number}</td>
-              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>{d.class}</td>
-              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>
-        <button className="delete-button" onClick={() => handleDelete(d.id)}>Delete</button>
-      </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    </div>
     </div>
   );
 }
