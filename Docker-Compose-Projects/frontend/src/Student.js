3200d397357158ea82cd80b9cdcf072c68118dd6
Sohail Mukhtiar <sohailmukhtiarali@gmail.com>
2026-02-04 18:18:54 +0500

    Update Student.js

--- Docker-Compose-Projects/frontend/src/Student.js
+++ Docker-Compose-Projects/frontend/src/Student.js
@@ -12,7 +12,7 @@ function Student() {
 
   const [data, setData] = useState([]);
 
-  const API_BASE_URL = process.env.REACT_APP_API_URL;
+  const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
   console.log("API:", API_BASE_URL);
 
   // Fetch students
