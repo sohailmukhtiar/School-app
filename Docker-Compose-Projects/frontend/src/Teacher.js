7044b91438373a2a1ae8309bd793e7612d954095
Sohail Mukhtiar <sohailmukhtiarali@gmail.com>
2026-02-04 18:29:42 +0500

    Update Teacher.js

--- Docker-Compose-Projects/frontend/src/Teacher.js
+++ Docker-Compose-Projects/frontend/src/Teacher.js
@@ -12,7 +12,7 @@ function Teacher() {
 
   const [data, setData]= useState([])
 
-  const API_BASE_URL = "/api";
+  const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
   const getData=()=> {
     fetch(`${API_BASE_URL}/teacher`)
